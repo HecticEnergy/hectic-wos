234fae75901c3947bae8cde86c23d5b17699cd2d
HecticEnergy <energy.hectic@gmail.com>
2025-02-24 09:02:23 -0600

    index styles

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-card>
+  <v-card rounded class="ma-10 pa-5">
     <v-card-title>Hectic WOS!</v-card-title>
-    <v-card-text>
+    <v-card-text class="pt-5">
       <v-row>
         <v-col>
           Welcome! Please make a selection from the menu.
