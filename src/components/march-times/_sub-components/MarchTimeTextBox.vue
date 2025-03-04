0e836c83385df350ba4963de231cf562a50176a6
HecticEnergy <energy.hectic@gmail.com>
2025-03-04 09:17:17 -0600

    marchTimeTextBox - remove "correctFormat" error, nicely handle bad input

--- src/components/march-times/_sub-components/MarchTimeTextBox.vue
+++ src/components/march-times/_sub-components/MarchTimeTextBox.vue
@@ -4,8 +4,6 @@
     label="March Time"
     width="105px"
     inputmode="numeric"
-    pattern="[0-9]{2}:[0-9]{2}:[0-9]{2}"
-    @update:model-value.capture="(e) => { console.log('modelUpdate', e)}"
     @blur="updateModel"
     @focus="$event.target.select()"
   />
@@ -31,6 +29,7 @@ type TimeModel = {
 const model = defineModel<TimeModel>({ required: true });
 onMounted(() => {
   marchTime.value = formatTimeMS(model.value.minutes, model.value.seconds ?? 0);
+  lastValidValue = "" + marchTime.value;
   updateModel();
 });
 
@@ -39,15 +38,27 @@ onMounted(() => {
 //   updateModel();
 // });
 
+let lastValidValue: string;
 const marchTime = ref<string | null>(null);
 
 const updateModel = () => {
   //assume that marchTime is MMSS or MM:SS or SS
+  //   console.log(
+  //     "updateModel",
+  //     marchTime.value,
+  //     Number.isNaN(Number.parseInt(marchTime.value ?? ""))
+  //   );
   if (marchTime.value === null) return;
+  if (Number.isNaN(Number.parseInt(marchTime.value))) {
+    marchTime.value = lastValidValue;
+    throw new Error("Invalid time format, numeric values only");
+  }
   const time = { hours: 0, minutes: 0, seconds: 0 };
-  const marchTimeValue = marchTime.value.padStart(4, "0");
-  if (marchTime.value.length > 2) {
-    if (marchTime.value.includes(":")) {
+  const marchTimeValue = marchTime.value
+    .replace(/[^0-9:]*/g, "")
+    .padStart(4, "0");
+  if (marchTimeValue.length > 2) {
+    if (marchTimeValue.includes(":")) {
       const parts = marchTimeValue.split(":");
       if (parts.length > 2) {
         throw new Error("Invalid time format only supports MM:SS");
@@ -59,9 +70,13 @@ const updateModel = () => {
       time.seconds = parseInt(marchTimeValue.substring(2, 4));
     }
   }
+
   const timeFromSec = getTimeFromSeconds(getSecondsFromTime(time));
   marchTime.value = formatTimeMS(timeFromSec.minutes, timeFromSec.seconds);
-  model.value = { ...model.value, ...timeFromSec };
+  lastValidValue = "" + marchTime.value;
+  model.value.minutes = time.minutes;
+  model.value.seconds = time.seconds;
   emit("update:model-value", model.value);
+  //   console.log("updateModel", marchTime.value, model.value);
 };
 </script>
