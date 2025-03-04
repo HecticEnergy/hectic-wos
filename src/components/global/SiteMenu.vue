1225716408b7b9d607e2a1a67ee87bf49c2b9077
HecticEnergy <energy.hectic@gmail.com>
2025-03-04 07:27:26 -0600

    add menu subtitle

--- src/components/global/SiteMenu.vue
+++ src/components/global/SiteMenu.vue
@@ -2,8 +2,10 @@
   <v-app-bar :density="smAndDown ? 'compact' : 'comfortable'">
     <!-- <v-btn flat icon="mdi-home" to="/" /> -->
     <v-app-bar-nav-icon @click.stop="toggleRail" />
-    <v-toolbar-title>Hectic WOS</v-toolbar-title>
-    <v-spacer />
+    <v-toolbar-title>
+      Hectic WOS &nbsp;
+      <span style="opacity: 0.6; font-size: small">{{ subTitle }}</span>
+    </v-toolbar-title>
     <v-divider vertical />
     <BuyMeBeer />
   </v-app-bar>
@@ -35,6 +37,7 @@
           :title="subItem.title"
           :prepend-icon="subItem.icon"
           :data-tour="subItem.dataTour"
+          @click="setSubTitle(subItem.subTitle)"
         />
         <v-divider v-if="item.divider" />
         <v-spacer />
@@ -49,15 +52,16 @@
     <template #append>
       <v-divider />
       <template v-for="item in bottomMenuItems" :key="item.label">
-        <v-list-subheader v-if="!rail" :title="item.label"  />
+        <v-list-subheader v-if="!rail" :title="item.label" />
         <v-list-item
-            v-for="subItem in item.items"
-            :key="subItem.key"
-            :to="subItem.to"
-            :title="subItem.title"
-            :value="subItem.title"
-            :prepend-icon="subItem.icon"
-          />
+          v-for="subItem in item.items"
+          :key="subItem.key"
+          :to="subItem.to"
+          :title="subItem.title"
+          :value="subItem.title"
+          :prepend-icon="subItem.icon"
+          @click="setSubTitle(subItem.subTitle)"
+        />
         <v-divider v-if="item.divider" />
       </template>
       <!--
@@ -94,6 +98,7 @@ const { smAndDown } = useDisplay();
 const rail = defineModel<boolean>("rail", { required: true });
 
 const drawer = ref(false);
+const subTitle = ref<string | undefined>();
 
 const toggleRail = () => {
   rail.value = !rail.value;
@@ -108,6 +113,13 @@ const setupMenuRail = () => {
   }
 };
 
+watch(route, (to, from) => {
+  console.debug("Route Changed", to, from);
+  setupMenuRail();
+});
+
+setupMenuRail();
+
 const menuItems = ref([
   {
     label: "",
@@ -119,6 +131,7 @@ const menuItems = ref([
         to: RouteHelper.HOME,
         key: "home-menu-item",
         dataTour: "home-navbar-link",
+        subTitle: undefined,
       },
       {
         title: "March Times",
@@ -126,14 +139,14 @@ const menuItems = ref([
         to: RouteHelper.MARCH_TIME,
         key: "march-time-new-menu-item",
         dataTour: "march-times-navbar-link",
+        subTitle: "March Times",
       },
     ],
   },
 ]);
 
 const bottomMenuItems = ref([
- 
-{
+  {
     label: "Under Construction",
     divider: false,
     items: [
@@ -143,17 +156,23 @@ const bottomMenuItems = ref([
         to: RouteHelper.MINISTRY_APPOINTMENTS,
         key: "ministry-appointments-menu-item",
         dataTour: "ministry-appointments-navbar-link",
+        subTitle: "Min App",
       },
     ],
-  }
+  },
 ]);
 
-watch(route, (to, from) => {
-  console.debug("Route Changed", to, from);
-  setupMenuRail();
-});
+const setSubTitle = (title: string | undefined) => {
+  subTitle.value = title;
+};
 
-setupMenuRail();
+const subtitle = menuItems.value
+  .concat(bottomMenuItems.value)
+  .find((i) => i.items.find((j) => j.to === route.path))
+  ?.items.find((i) => i.to === route.path)?.subTitle;
+if (subtitle) {
+  subTitle.value = subtitle;
+}
 
 // const toggleTheme = () => {
 //   localStorage.theme = theme.global.name.value =
