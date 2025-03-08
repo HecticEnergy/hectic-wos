593bc41e8b8ad46a010bdee7be5e7a5440bb98fe
HecticEnergy <energy.hectic@gmail.com>
2025-03-07 18:36:04 -0600

    draggable component I forgot last commit!

---
+++ src/components/global/sub-components/Draggable.vue
@@ -0,0 +1,20 @@
+<template>
+  <vue-draggable v-bind="$attrs" v-model="model" :handle="handle" element="span">
+    <template #item="{ element }">
+      <span>
+        <slot name="item" v-bind="{ element }"></slot>
+      </span>
+    </template>
+  </vue-draggable>
+</template>
+
+<script setup lang="ts" generic="T">
+import vueDraggable from "vuedraggable";
+
+const model = defineModel<T[]>({ required: true });
+defineProps<{ handle?: string }>();
+
+defineSlots<{
+  item(props: { element: T }): void;
+}>();
+</script>
