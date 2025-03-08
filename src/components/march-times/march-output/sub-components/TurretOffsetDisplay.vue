ab614a0a047bbcc1b05ae58f88bead48070369cb
HecticEnergy <energy.hectic@gmail.com>
2025-03-07 23:45:29 -0600

    Turret Offset Display init

---
+++ src/components/march-times/march-output/sub-components/TurretOffsetDisplay.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-row>
+    <v-col cols="auto">
+      <v-btn
+        prepend-icon="mdi-chess-rook"
+        title="Reset Turret Timer"
+        color="secondary"
+        text="Reset Timer"
+      />
+    </v-col>
+    <v-col>
+      <v-row class="d-flex flex-column">
+        <v-col align="right">
+          <utc-time :time="turretOffset" label="Turret Offset" />
+        </v-col>
+        <v-col align="right">
+          <utc-time :time="turretCountdown" label="Turret Count Down" />
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-row>
+</template>
+
+<script setup lang="ts">
+const turretOffset = ref({ hours: 0, minutes: 0, seconds: 0 });
+const turretCountdown = ref({ hours: 0, minutes: 0, seconds: 0 });
+
+</script>
