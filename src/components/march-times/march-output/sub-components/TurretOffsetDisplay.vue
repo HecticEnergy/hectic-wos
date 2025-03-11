43ceb05a2ec1e2ccc0bb8094eba2529b4731d8cf
HecticEnergy <energy.hectic@gmail.com>
2025-03-11 02:01:11 -0500

    turret timer functions working (not integrated into march)

--- src/components/march-times/march-output/sub-components/TurretOffsetDisplay.vue
+++ src/components/march-times/march-output/sub-components/TurretOffsetDisplay.vue
@@ -1,35 +1,56 @@
 <template>
   <v-row>
-    <v-col cols="auto">
-      <v-btn
-        prepend-icon="mdi-chess-rook"
-        title="Reset Turret Timer"
+    <v-col cols="12">
+      <v-icon icon="mdi-chess-rook" />
+      <v-icon icon="mdi-clock" />
+      <utc-time :time="turretCountdown" label="" />
+      <v-icon
+        icon="mdi-refresh"
+        title="Restart Turret Timer"
         color="secondary"
-        text="Reset Timer"
+        @click="resetTurretTimer"
       />
     </v-col>
     <v-col>
-      <v-row class="d-flex flex-column">
-        <v-col align="right">
-          <utc-time :time="turretOffset" label="Turret Offset" />
-        </v-col>
-        <v-col align="right">
-          <utc-time :time="turretCountdown" label="Turret Count Down" />
-        </v-col>
-      </v-row>
+      <utc-time :time="turretOffset" label="Turret Offset" />
     </v-col>
   </v-row>
 </template>
 
 <script setup lang="ts">
+import type { Time } from "@/models";
+import { Clock } from "@/services/time-helpers/clock-logic";
+import {
+  getSecondsFromTime,
+  getTimeFromSeconds,
+  getUtcTime,
+} from "@/services/time-helpers";
+
+//Thats done.. now hit on the second with turret Offset
 const turretOffset = ref({ hours: 0, minutes: 0, seconds: 0 });
 const turretCountdown = ref({ hours: 0, minutes: 0, seconds: 0 });
 
-//TODO: Implement turret timer reset
-//  use clock-logic
-//  for "getTime", calculate the difference between the current time and the turret offset
-//  count down
-//  reset when the timer reaches 0 (or lower)
-
 // Store Turret Time in local storage
+
+const resetTurretTimer = () => {
+  const time = getUtcTime(new Date());
+  turretOffset.value = { hours: 0, minutes: 0, seconds: time.seconds };
+  turretCountdown.value = { hours: 0, minutes: 1, seconds: 0 };
+  // clock.restartClock();
+};
+
+const setClock = (time: Time) => {
+  turretCountdown.value = time;
+};
+const getTime = () => {
+  const seconds = getSecondsFromTime(turretCountdown.value);
+  if (seconds <= 0) return { hours: 0, minutes: 1, seconds: 0 };
+  return getTimeFromSeconds(seconds - 1);
+};
+
+const clock = new Clock(setClock, getTime);
+
+onUnmounted(() => {
+  clock.destroy();
+});
 </script>
