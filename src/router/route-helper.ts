999ff1a1d2f8a1259dba47f303ec39f6d7fdac4c
HecticEnergy <energy.hectic@gmail.com>
2025-03-10 20:54:12 -0500

    route-helpers - use consts ditch enum

--- src/router/route-helper.ts
+++ src/router/route-helper.ts
@@ -1,71 +1,17 @@
-import type { Router } from "vue-router";
+const RouteConsts = {
+  HOME: "/",
+  MARCH_TIME: "/MarchTimesPage",
+  CREATE_MEMBER_SINGLE: "/march-time-pages/CreateMemberPage",
+  IMPORT_CREATED_MEMBER: "/march-time-pages/ImportCreatedMemberPage",
 
-enum Routes {
-  HOME = "/",
-  MARCH_TIME = "/MarchTimesPage",
-  CREATE_MEMBER_SINGLE = "/march-time-pages/CreateMemberPage",
-  IMPORT_CREATED_MEMBER = "/march-time-pages/ImportCreatedMemberPage",
+  MINISTRY_APPOINTMENTS: "/MinistryAppointments",
 
-  MINISTRY_APPOINTMENTS = "/MinistryAppointments",
-
-  TEST_PAGE = "/test-pages/Test",
-}
-
-const HOME = Routes.HOME;
-const MARCH_TIME = Routes.MARCH_TIME;
-const CREATE_MEMBER_SINGLE = Routes.CREATE_MEMBER_SINGLE;
-const IMPORT_CREATED_MEMBERS = Routes.IMPORT_CREATED_MEMBER;
-
-const MINISTRY_APPOINTMENTS = Routes.MINISTRY_APPOINTMENTS;
-
-const TEST_PAGE = Routes.TEST_PAGE;
-
-// const formatPrintPuppyCertificateUrl = (pupIds: string[]) => {
-//   const queryString = pupIds.toString().replace(/"|\[|\]/g, '');
-//   return `${PRINT}?view=pup&pupIds=${queryString}`;
-// };
-
-// const unformatPrintPuppyCertificateUrl = (
-//   pupIdsQueryString: string
-// ): string[] => {
-//   const pupIds = `["${pupIdsQueryString.toString().split(',').join('","')}"]`;
-//   return JSON.parse(pupIds);
-// };
-
-const openInNewTab = (router: Router, to: string): boolean => {
-  const openNewTab = localStorage.getItem("newTabsDisabled");
-  if (openNewTab === null || openNewTab === "false") {
-    const routeData = router.resolve(to);
-    window.open(routeData.href, "_blank");
-    const showAlert = localStorage.getItem("showPopupEnableAlert");
-
-    if (showAlert === null || showAlert === "true") {
-      const continueToShowAlert = confirm(
-        "Did the new tab open? - Print view should be opened in a new tab. If not, you may need to enable popups for this site." +
-          " - 'Ok' to close this and continue. - 'Cancel' to stay here and try again."
-      );
-      localStorage.setItem("showPopupEnableAlert", `${!continueToShowAlert}`);
-      return !continueToShowAlert;
-    }
-  } else {
-    router.push(to);
-    return true;
-  }
-
-  return true;
+  TEST_PAGE: "/test-pages/TestPage",
+  COLORS_PAGE: "/test-pages/ColorsPage",
+  TRANSITIONS_PAGE: "/test-pages/TransitionTestPage",
+  DRAGGABLE_CHIPS_PAGE: "/test-pages/TestDraggableChipsPage",
 };
 
 export default {
-  HOME,
-  MARCH_TIME,
-  CREATE_MEMBER_SINGLE,
-  IMPORT_CREATED_MEMBERS,
-
-
-  MINISTRY_APPOINTMENTS,
-
-  TEST_PAGE,
-  // formatPrintPuppyCertificateUrl,
-  // unformatPrintPuppyCertificateUrl,
-  openInNewTab,
+  ...RouteConsts,
 };
