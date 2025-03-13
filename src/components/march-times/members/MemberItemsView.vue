fcedbdf5e5341b641581212dd152f539dc5c65f5
HecticEnergy <energy.hectic@gmail.com>
2025-03-12 21:46:03 -0500

    Member Item View fixes - was not refreshing when store changed. Chips on main screen should now click and respond.

--- src/components/march-times/members/MemberItemsView.vue
+++ src/components/march-times/members/MemberItemsView.vue
@@ -1,12 +1,18 @@
 <template>
-  <draggable v-model="allSelectMembers" item-key="id" handle=".move-handle">
+  <draggable
+    v-model="model"
+    item-key="id"
+    handle=".move-handle"
+    @start="start"
+    @end="draggingMemberId = null"
+  >
     <template #item="{ element }">
       <div>
         <div
           :class="
-            `my-2 rounded` +
-            (element.isDragging ? ' on-hover' : ' ') +
-            (element.member.isSelected ? ' bg-primary' : ' bg-surface-variant')
+            `my-2 rounded cursor-pointer` +
+            (draggingMemberId === element.id ? ' on-hover' : ' ') +
+            (element.isSelected ? ' bg-primary' : ' bg-surface-variant')
           "
         >
           <v-row
@@ -15,21 +21,13 @@
             @click="toggleMemberSelected(element)"
           >
             <v-col cols="auto">
-              <v-icon
-                icon="mdi-drag"
-                size="large"
-                class="move-handle"
-                @touchstart="move(element, true)"
-                @touchend="move(element, false)"
-                @mousedown="move(element, true)"
-                @mouseup="move(element, false)"
-              />
+              <v-icon icon="mdi-drag" size="large" class="move-handle" />
             </v-col>
             <v-col>
               <label>{{ element.name }}</label>
             </v-col>
             <v-col cols="auto" class="mr-2">
-              <label>{{ element.marchTime }}</label>
+              <label>{{ getMemberMarchTimeDisplay(element) }}</label>
             </v-col>
             <v-col cols="auto">
               <v-icon icon="mdi-pencil" @click.stop="edit(element)" />
@@ -42,7 +40,6 @@
 </template>
 
 <script setup lang="ts">
-import draggable from "vuedraggable";
 import { formatTimeMS } from "@/services/time-helpers/time-formatters";
 import type { Member } from "@/models";
 import { getMemberMarchTime } from "@/services/target-logic";
@@ -62,75 +59,40 @@ const emit = defineEmits<{
   (event: "save", members: Member): void;
   (event: "edit", member: Member): void;
   (event: "update"): void;
+  (event: "update:model-value", value: Member[]): void;
 }>();
 
-type SelectMember = {
-  id: number;
-  isDragging: boolean;
-  name: string;
-  marchTime: string | undefined;
-  member: Member;
-};
-
-const _allMembers = ref<SelectMember[] | undefined>(undefined);
+const draggingMemberId = ref<number | null>(null);
 
 const allSelectMembers = computed({
-  get: () => _allMembers.value,
-  set: (value: SelectMember[]) => {
-    const updateMembers: Member[] = [];
-    let order = 0;
-    value.forEach((m) => {
-      const newOrder = (order += 10);
-      if (m.member.order !== newOrder) {
-        m.member.order = newOrder;
-        updateMembers.push(m.member);
-      }
-    });
-    _allMembers.value = value;
-    updateMembers.forEach((m) => emit("save", m));
-    emit("update");
+  get: () => model.value,
+  set: (value: Member[]) => {
+    changeOrder(value.map((m) => m));
   },
 });
-const isDragging = ref<boolean>(false);
 
-onMounted(() => {
-  loadAllMembers();
-});
-
-const loadAllMembers = () => {
-  allSelectMembers.value = model.value
-    .sort((m) => m.order)
-    .map((member) => {
-      return {
-        id: member.id,
-        name: member.name,
-        marchTime: getMemberMarchTimeDisplay(member),
-        member,
-      } as SelectMember;
-    });
+const start = (e: { item: { _underlying_vm_: { id: number } } }) => {
+  draggingMemberId.value = e.item._underlying_vm_.id;
 };
 
-const move = (element: SelectMember, isMouseDown: boolean) => {
-  isDragging.value = isMouseDown;
-  element.isDragging = isMouseDown;
-  allSelectMembers.value?.forEach((m) => {
-    if (m.id !== element.id) {
-      m.isDragging = false;
-    }
+const changeOrder = (members: Member[]) => {
+  const updateMembers: Member[] = [];
+  let order = 0;
+  members.forEach((m) => {
+    const newOrder = (order += 10);
+    m.order = newOrder;
+    updateMembers.push(m);
   });
+  emit("update");
 };
 
-const toggleMemberSelected = (element: SelectMember) => {
-  element.member.isSelected = !element.member.isSelected;
-  const modelMember = model.value.find((m) => m.id === element.id);
-  if (modelMember) {
-    modelMember.isSelected = element.member.isSelected;
-    emit("update");
-  }
+const toggleMemberSelected = (element: Member) => {
+  element.isSelected = !element.isSelected;
+  emit("update:model-value", allSelectMembers.value);
 };
 
-const edit = (member: SelectMember) => {
-  emit("edit", member.member);
+const edit = (member: Member) => {
+  emit("edit", member);
 };
 
 const getMemberMarchTimeDisplay = (member: Member) => {
